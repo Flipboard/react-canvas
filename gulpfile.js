a9aed13c0fa49170b1c02f8c6636f4f94d2c79df
Dan Abramov <dan.abramov@gmail.com>
2015-02-11 00:34:32 +0300

    Use React Hot Loader for live-editing examples

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
-var connect = require('gulp-connect');
-var webpack = require('gulp-webpack');
+var webpack = require('webpack');
+var webpackPlugin = require('gulp-webpack');
 var webpackConfig = require('./webpack.config.js');
+var WebpackDevServer = require('webpack-dev-server');
 
 gulp.task('clean', function () {
   return gulp.src('build', {read: false})
@@ -11,23 +12,22 @@ gulp.task('clean', function () {
 
 gulp.task('build', function () {
   return gulp.src(webpackConfig.entry.timeline[0])
-    .pipe(webpack(webpackConfig))
+    .pipe(webpackPlugin(webpackConfig))
     .pipe(gulp.dest('build/'));
 });
 
 gulp.task('serve', function () {
-  connect.server({
-    livereload: true
+  new WebpackDevServer(webpack(webpackConfig), {
+    hot: true,
+    publicPath: webpackConfig.output.publicPath
+  }).listen(8080, 'localhost', function (err) {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log('Development server listening at http://localhost:8080/examples');
   });
 });
 
-gulp.task('reload-js', function () {
-  return gulp.src('./build/*.js')
-    .pipe(connect.reload());
-});
 
-gulp.task('watch', function () {
-  gulp.watch(['./build/*.js'], ['reload-js']);
-});
-
-gulp.task('default', ['clean', 'build', 'serve', 'watch']);
+gulp.task('default', ['clean', 'build', 'serve']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
-module.exports = {
-  cache: true,
+var webpack = require('webpack');
 
-  watch: true,
+var hotServers = [
+  'webpack-dev-server/client?http://localhost:8080',
+  'webpack/hot/only-dev-server'
+];
+
+module.exports = {
+  devtool: 'eval',
 
   entry: {
-    'listview': ['./examples/listview/app.js'],
-    'timeline': ['./examples/timeline/app.js'],
-    'css-layout': ['./examples/css-layout/app.js']
+    'listview': ['./examples/listview/index'].concat(hotServers),
+    'timeline': ['./examples/timeline/index'].concat(hotServers),
+    'css-layout': ['./examples/css-layout/index'].concat(hotServers)
   },
 
   output: {
-    filename: '[name].js'
+    filename: '[name].js',
+    publicPath: '/build/'
   },
 
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader!transform/cacheable?envify' },
+      { test: /\.js$/, loader: 'react-hot!jsx-loader!transform/cacheable?envify' },
     ]
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
 
   resolve: {
     root: __dirname,
