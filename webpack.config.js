b9a9a0e18c9f827cacfca7ccfe5baac725ad943a
April Arcus <april.arcus@gmail.com>
2015-02-16 13:21:04 -0800

    text example

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: {
     'listview': ['./examples/listview/app.js'],
     'timeline': ['./examples/timeline/app.js'],
-    'css-layout': ['./examples/css-layout/app.js']
+    'css-layout': ['./examples/css-layout/app.js'],
+    'text': ['./examples/text/app.js']
   },
 
   output: {
